da2db80a729c27737f0318f717f5dc92d9cf8a04
Nico-Serra <nico.serra000@gmail.com>
2024-07-17 19:04:21 +0200

    add input in main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
+  value = 'Clear me'
 }
