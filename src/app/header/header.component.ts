9064a791c77b57e72c4d02db2cb647189571e44b
Nico-Serra <nico.serra000@gmail.com>
2024-07-17 18:47:17 +0200

    add header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  menu = ['home', 'about', 'contacts'];
+  socials = [`linkedin`, 'github', 'youtube']
 }
