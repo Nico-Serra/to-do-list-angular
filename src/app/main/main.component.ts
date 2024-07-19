0e761985f5f142e8fd7253f12dba1a9e69a14976
Nico-Serra <nico.serra000@gmail.com>
2024-07-19 12:23:36 +0200

    Add input for write toDo, add localeStorage for save the data and add methods for remove toDo

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,17 +1,61 @@
 import { Component } from '@angular/core';
-import { MatInputModule } from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule} from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { LocalStorageService } from '../local-storage.service';
+import { MatCardModule } from '@angular/material/card';
+import { log } from 'console';
+
+
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule],
+  imports: [FormsModule, MatCardModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-  value = 'Clear me'
+
+  constructor(private localStorageService: LocalStorageService) { }
+
+  list: string[] = []
+  dynamicValue = ''
+  listPrint: string[] | null = null
+
+  ngOnInit() {
+    this.listPrint = this.localStorageService.getItem('list');
+    console.log("prsi");
+    console.log(this.localStorageService.getItem('list'));
+    if (this.localStorageService.getItem('list')) {
+      this.list = this.localStorageService.getItem('list');
+    }
+
+  }
+
+
+  addToDo() {
+    let toDo = ''
+    toDo = this.dynamicValue
+    this.dynamicValue = ''
+    this.list.push(toDo)
+    console.log(this.list);
+    let myListString = JSON.stringify(this.list);
+    localStorage.setItem('list', myListString);
+    this.listPrint = this.localStorageService.getItem('list');
+  }
+
+  removeToDo(index: number) {
+    console.log(index);
+    this.list.splice(index, 1)
+    let myListString = JSON.stringify(this.list);
+    localStorage.setItem('list', myListString);
+    this.listPrint = this.localStorageService.getItem('list');
+
+
+  }
+
+  removeAll() {
+    localStorage.clear()
+    this.listPrint = []
+    this.list = []
+  }
 }
